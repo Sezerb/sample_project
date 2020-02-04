56a7d8520c5eff9372b6713f6b3d25b37161de98
sezerb <sezerbaglan@gmail.com>
2020-02-04 23:54:03 +0300

    make mongodb work

--- app.js
+++ app.js
@@ -4,21 +4,28 @@ const message = require('./message.js');
 const mustache = require('mustache-express');
 const mongo = require('mongodb');
 const mongoClient = mongo.MongoClient;
-const url = 'mongodb://127.0.0.1:27017/foo'
+const mongoUrl = 'mongodb://127.0.0.1:27017';
 
-//connect to a mongo db.
-// mongoClient.connect(url, function(error,db){
-//     if(error){
-//         console.log(error)
-//     }else{
-//         let animals = db.collection('animals');
-//         animals.insert([{name:"pal",type:"terrier"}]);
-//         animals.find({}).toArray(function(err,result){
-//             console.log(JSON.stringify(results));
-//         })
-//         db.close();
-//     }
-// })
+//Start mongdb server: mongod --dbpath /usr/local/var/mongodb --logpath /usr/local/var/log/mongodb/mongo.log --fork
+
+mongoClient.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true}, function(error, client){
+    var db = client.db('mydb');
+    let cars = db.collection('cars');
+    
+    //cars.insertOne({name:"Honda", model:"CRV"});
+    //cars.deleteOne({model:"CRV"});
+
+    cars.find({}).toArray(function(err, docs) {
+
+     // Print the documents returned
+     docs.forEach(function(doc) {
+         console.log(doc);
+     });
+
+     // Close the DB
+     client.close();
+     });
+});
 
 //import some data from message module.
 console.log(message["letters"]);
