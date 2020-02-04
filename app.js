f414508e44f221cfe40d55600c067c7dc9eae268
sezerb <sezerbaglan@gmail.com>
2020-02-04 08:40:23 +0300

    mongodb experiment

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const express = require('express');
 const app = express();
 const message = require('./message.js');
 const mustache = require('mustache-express');
-let all_letters;
+const mongo = require('mongodb');
+const mongoClient = mongo.MongoClient;
+const url = 'mongodb://127.0.0.1:27017/foo'
 
+//connect to a mongo db.
+// mongoClient.connect(url, function(error,db){
+//     if(error){
+//         console.log(error)
+//     }else{
+//         let animals = db.collection('animals');
+//         animals.insert([{name:"pal",type:"terrier"}]);
+//         animals.find({}).toArray(function(err,result){
+//             console.log(JSON.stringify(results));
+//         })
+//         db.close();
+//     }
+// })
+
+//import some data from message module.
 console.log(message["letters"]);
 
 //Serve public static files.
@@ -17,19 +34,6 @@ app.set('views', __dirname + '/views');
 app.get("/", function(req,res){
     res.render("index");
 })
-
-// app.get("/", function(request,response){
-//     for(let i=0; i<message["letters"].length; i++){
-//         all_letters += message["letters"][i] + "<br/>";
-//     }
-//     response.send(all_letters);
-//     //response.send("<h1 style='text-align:center'>Hey Friends, welcome to my app</h1>");
-// })
-
-// app.get("/users/:name", function(req, res){
-//     //res.send(req.params.name);
-//     res.send(req["params"]["name"]);
-// })
 
 app.listen(3000,function(error){
     if(error == true){
