a5313f9d87a540ffc8ce7345a8dcdc191da7a992
sezerb <sezerbaglan@gmail.com>
2020-02-07 00:03:44 +0300

    aes experiment

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ const mustache = require('mustache-express');
 const mongo = require('mongodb');
 const mongoClient = mongo.MongoClient;
 const mongoUrl = 'mongodb://127.0.0.1:27017';
+const crypto = require('crypto');
+const ENC_KEY = "b265da80b661fa703388887a27c229ff"; // set random encryption key
+const IV = "aa624696d5303317"; // set random initialisation vector
+const phrase = "Seco";
+
+var encrypt = ((val) => {
+    let cipher = crypto.createCipheriv('aes-256-cbc', ENC_KEY, IV);
+    let encrypted = cipher.update(val, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+    return encrypted;
+  });
+
+var decrypt = ((encrypted) => {
+    let decipher = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, IV);
+    let decrypted = decipher.update(encrypted, 'base64', 'utf8');
+    return (decrypted + decipher.final('utf8'));
+});
+
+encrypted_key = encrypt(phrase);
+decrypted_key = decrypt(encrypted_key);
+console.log("Encrypted:", encrypted_key);
+console.log("Decrypted:", decrypted_key);
+
+newKey = crypto.randomBytes(16).toString('hex');
+newIv = crypto.randomBytes(8).toString('hex');
+console.log("newKey:", newKey);
+console.log("newIv:", newIv);
 
 //Start mongdb server: mongod --dbpath /usr/local/var/mongodb --logpath /usr/local/var/log/mongodb/mongo.log --fork
 
